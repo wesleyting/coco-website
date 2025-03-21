d4fa3e55e0785fde330a2abfc255567f77343e55
Wesley Ting <wesleyting3@gmail.com>
2025-03-20 23:19:48 -0700

    Updated page with every section

--- app/page.js
+++ app/page.js
@@ -1,13 +1,28 @@
 import Image from "next/image";
 import Header from "../components/Header";
 import Hero from "../components/Hero";
+import Fruits from "../components/Fruits";
+import Menu from "../components/Menu";
+import AboutUsSection from "../components/AboutUsSection";
+import LocationSection from "../components/LocationSection";
+import Footer from "../components/Footer";
 
 export default function Home() {
   return (
     <div>
       <main>
-        <Header />
-        <Hero />
+        <section className="relative bg-turquoise overflow-hidden px-4">
+          <div className="relative max-w-[1400px] mx-auto">
+            <Fruits />
+            <Header />
+            <Hero />
+          </div>
+        </section>
+
+        <Menu />
+        <AboutUsSection />
+        <LocationSection />
+        <Footer />
       </main>
     </div>
   );
