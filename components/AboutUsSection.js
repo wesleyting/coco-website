a9dd2764a0b60008f91db37dd030d6fc77e58fbd
Wesley Ting <wesleyting3@gmail.com>
2025-03-20 23:39:21 -0700

    fixed about page characters

--- components/AboutUsSection.js
+++ components/AboutUsSection.js
@@ -27,9 +27,9 @@ export default function AboutUsSection() {
         <p className="text-lg md:text-xl xl:text-2xl leading-relaxed text-justify px-6 sm:px-6">
           Welcome to CoCo - your go-to spot for refreshing drinks and delicious
           snacks! Located right inside Summer Mall, we proudly serve walk-in
-          customers who want a quick pick-me-up, a cozy place to chill, or
-          something tasty to-go. Whether you're in the mood for icy fruit teas,
-          creamy bubble milk tea, or crispy fried bites, we've got something for
+          customers looking for a quick pick-me-up, a cozy place to chill, or
+          something tasty to-go. Whether you're craving icy fruit teas, creamy
+          bubble milk tea, or crispy fried bites - we have something for
           everyone.
         </p>
       </div>
