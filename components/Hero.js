c36a566918e8ff339b26caa0a6a006f3d6261cf5
Wesley Ting <wesleyting3@gmail.com>
2025-03-20 23:19:18 -0700

    Hero Section Complete

--- components/Hero.js
+++ components/Hero.js
@@ -6,29 +6,37 @@ const lobster = Lobster({ subsets: ["latin"], weight: "400" });
 
 export default function Hero() {
   return (
-    <section className="w-full bg-turquoise relative overflow-hidden">
-      <div className="relative max-w-xl sm:max-w-3xl md:max-w-3xl lg:max-w-6xl mx-auto px-4 pb-10 md:py-24 grid grid-cols-1 md:grid-cols-2 items-center gap-12 md:gap-8">
-        {/* Left: Text */}
-        <div className="relative z-20 text-center md:text-left max-w-md mx-auto md:mx-0">
+    <section className="w-full relative overflow-hidden  md:px-4 select-none">
+      <div className="relative max-w-[1300px] mx-auto md:h-[500px] lg:h-[600px] xl:h-[700px]">
+        {/* Left: Text (absolute on desktop) */}
+        <div className="relative  md:absolute md:top-[42%] md:left-[5%] md:translate-y-[-50%] z-20 max-w-[900px] text-center md:text-left">
           <h1
-            className={`text-7xl md:text-8xl lg:text-7xl font-bold leading-tight text-white ${lobster.className}`}
+            className={`text-6xl sm:text-7xl lg:text-8xl  xl:text-[9rem] font-bold leading-tight text-white ${lobster.className}`}
           >
-            Enjoy It <span className="relative z-30 inline-block">Fresh</span>
+            Enjoy It Fresh
           </h1>
-          <p className="mt-4 text-2xl font-semibold text-orange max-w-md mx-auto md:mx-0 relative z-20">
+          <p className="mt-4 text-2xl lg:text-3xl lg:mt-8 font-semibold text-orange relative">
             Order Online Today!
           </p>
-          <button className="mt-6 px-6 py-4 text-xl bg-orange text-white rounded-lg font-semibold hover:bg-gray-800 transition relative z-20">
+          <button
+            onClick={() => {
+              const section = document.getElementById("location");
+              if (section) {
+                section.scrollIntoView({ behavior: "smooth" });
+              }
+            }}
+            className="mt-6 mb-8 lg:mt-20 px-8 py-4 lg:px-14 lg:py-6 text-xl lg:text-2xl bg-orange text-white rounded-lg font-semibold hover:bg-gray-800 transition relative z-20 cursor-pointer"
+          >
             Order Now
           </button>
         </div>
 
-        {/* Right: Image */}
-        <div className="relative z-10 md:absolute md:top-[30%] md:right-[-80px] md:w-[60%] lg:w-[50%] pointer-events-none">
+        {/* Right: Image (absolute on desktop) */}
+        <div className="relative md:absolute md:top-1/2 md:right-[0%] md:translate-y-[-50%] z-[30] max-w-[500px] md:max-w-[550px] lg:max-w-[70%] xl:max-w-[850px] mx-auto md:mx-0 ">
           <img
             src="/hero-image.png"
             alt="Hero Drink"
-            className="w-[100%] sm:w-[600px] lg:w-[500px] h-auto object-contain md:-translate-y-[20%]"
+            className="w-full h-auto object-contain"
           />
         </div>
       </div>
